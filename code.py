403cdc97dc622decd9b23fefc6b387bf24d25563
mahdi <mafarhang76@gmail.com>
2019-04-25 03:16:54 +0430

    little changes

--- code.py
+++ code.py
@@ -51,15 +51,14 @@ def train(data):
 				a, b = possibility_of_word(data[0:num_of_training_data], word)
 				spams[word] = a
 				hams[word] = b
-		if (i < num_of_training_data):
-			if (sentence[0] == 'spam'):
-				num_of_spam_in_size[len(sentence[1])] += 1
-			else:
-				num_of_ham_in_size[len(sentence[1])] += 1
+		if (i > num_of_training_data):
+			continue
+		if (sentence[0] == 'spam'):
+			num_of_spam_in_size[len(sentence[1])] += 1
+		else:
+			num_of_ham_in_size[len(sentence[1])] += 1
 		i += 1
 	return spams, hams, num_of_spam_in_size, num_of_ham_in_size
-
-
 
 def guess_type_of_sentence(spams, hams, words, num_of_spams, num_of_hams, spam_sizes, ham_sizes):
 	
@@ -72,53 +71,49 @@ def guess_type_of_sentence(spams, hams, words, num_of_spams, num_of_hams, spam_s
 	temp = len(words)
 	p_all_spams += temp / 3 * math.log(((spam_sizes[temp] if spam_sizes[temp] != 0 else 0.1) / num_of_spams), 2)
 	p_all_hams += temp / 3 * math.log(((ham_sizes[temp] if ham_sizes[temp] != 0 else 0.1) / num_of_hams), 2)
-	if (p_all_spams > p_all_hams):
+	if (p_all_spams > p_all_hams):	
 		return 'spam'
 	return 'ham'
+
+def test(data, spams, hams, spam_sizes, ham_sizes):
+	num_of_spam = 0
+	num_of_ham = 0
+	for item in data:
+		if (item[0] == 'spam'):
+			num_of_spam += 1
+		else:
+			num_of_ham += 1
+
+	result = []
+	for i in range(num_of_training_data, len(data)):
+		result.append(guess_type_of_sentence(spams, hams, data[i][1], num_of_spam, num_of_ham, spam_sizes, ham_sizes))
+	
+	total = len(result)
+	true_spam = 0
+	true_ham = 0
+	false_spam = 0
+	false_ham = 0
+
+	for i in range(total):
+		if (data[num_of_training_data + i][0] == 'spam'):
+			if (result[i] == 'spam'):
+				true_spam += 1
+			else:
+				false_spam += 1
+		else:
+			if (result[i] == 'ham'):
+				true_ham += 1
+			else:
+				false_ham += 1
+	print('number of test data = ', total)
+	print('Recall = ', true_spam / (true_spam + false_spam))
+	print('Precision = ', true_spam / (true_spam + false_ham))
+	print('Accuracy = ', (true_spam + true_ham) / total)
 
 t1 = time()
 data = getting_train_data('train_test.csv')
 spams, hams, spam_sizes, ham_sizes = train(data)
 t2 = time()
-print(t2-t1)
+print('training time ', t2 - t1)
 
-num_of_spam = 0
-num_of_ham = 0
-sum_spam = 0
-sum_ham = 0
-for item in data:
-	if (item[0] == 'spam'):
-		num_of_spam += 1
-		sum_spam += len(item[1])
-	else:
-		num_of_ham += 1
-		sum_ham += len(item[1])
-av_len_ham = float(sum_ham / num_of_ham)
-av_len_spam = float(sum_spam / num_of_spam)
-
-
-result = []
-for i in range(num_of_training_data, len(data)):
-	result.append(guess_type_of_sentence(spams, hams, data[i][1], num_of_spam, num_of_ham, spam_sizes, ham_sizes))
-print(len(result))
-
-true_spam = 0
-true_ham = 0
-false_spam = 0
-false_ham = 0
-total = len(result)
-
-for i in range(total):
-	if (data[num_of_training_data + i][0] == 'spam'):
-		if (result[i] == 'spam'):
-			true_spam += 1
-		else:
-			false_spam += 1
-	else:
-		if (result[i] == 'ham'):
-			true_ham += 1
-		else:
-			false_ham += 1
-print('Recall = ', true_spam / (true_spam + false_spam))
-print('Precision', true_spam / (true_spam + false_ham))
-print('Accuracy', (true_spam + true_ham) / total)
+test(data, spams, hams, spam_sizes, ham_sizes)
